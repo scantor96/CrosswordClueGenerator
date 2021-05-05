96f04feb84c299c09e3eb044439dcf4d7050ebba
scantor96 <56449758+scantor96@users.noreply.github.com>
2021-05-05 12:44:56 -0400

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+# author:haiton
+# datetime:18-11-14 ??10:22
+# FOR MODEL VOCABULARY
+PAD = '<pad>'
+UNK = '<unk>'
+BOS = '<s>'
+EOS = '</s>'
+PAD_IDX = 0
+UNK_IDX = 1
+BOS_IDX = 2
+EOS_IDX = 3
